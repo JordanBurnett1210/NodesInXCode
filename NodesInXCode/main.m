8192d7fb4d577b71c670fb314e25b61efdacc23a
JordanBurnett1210 <jabur1210@gmail.com>
2016-03-16 08:25:14 -0600

    Initial Commit

---
+++ NodesInXCode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NodesInXCode
+//
+//  Created by Burnett, Jordan on 3/16/16.
+//  Copyright © 2016 Burnett, Jordan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
